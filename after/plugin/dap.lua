b86fb25f394b06422952a47185e3a15427e2e39c
Icaro-Nunes <icarolimanunes@gmail.com>
2023-11-08 21:09:53 -0300

    moved dap config to its should place

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,19 +1,36 @@
 local dap = require("dap")
 
--- dap.adapters.cpp = {
---     type = "executable",
---     command = "~/.local/share/nvim/mason/packages/cpptools/extension/bin/cpptools",
---     name = "cpp"
--- }
--- 
--- dap.configurations.cpp = {
---     {
---         type = 'cpp';
---         request = 'launch';
---         name = 'Launch file';
---         program = '${fileDir}/out/m';
---     },
--- }
+-- setup mason and ensure cpptools is installed
+require("mason").setup()
+require("mason-nvim-dap").setup({
+  ensure_installed = {'cpptools'}
+})
+
+-- setup cpptools adapter
+dap.adapters.cpptools = {
+  type = 'executable';
+  name = "cpptools",
+  command = vim.fn.stdpath('data') .. '/mason/bin/OpenDebugAD7',
+  args = {},
+  attach = {
+    pidProperty = "processId",
+    pidSelect = "ask"
+  },
+}
+
+-- this configuration should start cpptools and the debug the executable main in the current directory when executing :DapContinue
+dap.configurations.cpp = {
+  {
+    name = "Launch",
+    type = "cpptools",
+    request = "launch",
+    program = '${workspaceFolder}/out/m',
+    cwd = '${workspaceFolder}',
+    stopOnEntry = true,
+    args = {"<", "${workspaceFolder}/cases/case"} ,
+    runInTerminal = false,
+  },
+}
 
 vim.keymap.set("n", "<leader>bp", dap.toggle_breakpoint)
 vim.keymap.set("n", "<leader>n", dap.step_over)
